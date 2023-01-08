83229fe9e5b0a09520054d3f265da7dcb6f42f2f
DemidovAV <demidovavspb@gmail.com>
2023-01-08 16:13:28 +0300

    git commit

--- lesson6/src/main/java/Lesson6Main.java
+++ lesson6/src/main/java/Lesson6Main.java
@@ -37,13 +37,19 @@ public class Lesson6Main {
             }
         });
         Thread count2 = new Thread(() -> {
-            for (int i = 0; i < arrayHalf2.length; i++) {
-                arrayHalf2[i] = (float) (arrayHalf2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                        Math.cos(0.4f + i / 2));
+            for (int i = 0, j = HALF; i < arrayHalf2.length; i++, j++) {
+                arrayHalf2[i] = (float) (arrayHalf2[i] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) *
+                        Math.cos(0.4f + j / 2));
             }
         });
         count1.start();
         count2.start();
+        try {
+            count1.join();
+            count2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.arraycopy(arrayHalf1, 0, array, 0, HALF);
         System.arraycopy(arrayHalf2, 0, array, HALF, HALF);
         long finish = System.currentTimeMillis();
