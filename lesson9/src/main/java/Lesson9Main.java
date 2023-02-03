7cc787e06d51fa5ddd9509b0789f880d807b157d
DemidovAV <demidovavspb@gmail.com>
2023-02-03 12:55:18 +0300

    git commit

--- lesson9/src/main/java/Lesson9Main.java
+++ lesson9/src/main/java/Lesson9Main.java
@@ -1,19 +1,24 @@
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.lang.reflect.*;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Lesson9Main {
 
     private static Statement statement;
+    private static PreparedStatement preparedStatement;
     private static Connection connection;
     public static void main(String[] args) {
+        Game witcher = new Game(1, "Witcher", GameType.SINGLE, 16);
+        Game wow = new Game(2, "World of Warcraft", GameType.MULTIPLAYER, 12);
         try {
             connect();
             createTable(Game.class);
+            addGame(witcher);
+            addGame(wow);
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -22,8 +27,12 @@ public class Lesson9Main {
     }
 
     public static void addGame(Game game) throws SQLException {
-        StringBuilder request = new StringBuilder("INSERT INTO ");
-        request.append(game.getClass().getAnnotation(Table.class).title() + " (");
+            preparedStatement = connection.prepareStatement("INSERT INTO Games (id, gameName, type, PG) VALUES (?, ?, ?, ?);");
+            preparedStatement.setObject(1, game.id);
+            preparedStatement.setObject(2, game.gameName);
+            preparedStatement.setObject(3, game.type);
+            preparedStatement.setObject(4, game.PG);
+            preparedStatement.executeUpdate();
     }
     public static void createTable(Class myClass) throws SQLException {
         Map<Class, String> sqlClasses = new HashMap<>();
@@ -35,7 +44,7 @@ public class Lesson9Main {
         if (!myClass.isAnnotationPresent(Table.class)) {
             throw new RuntimeException("No table found");
         }
-        StringBuilder request = new StringBuilder("CREATE TABLE ");
+        StringBuilder request = new StringBuilder("CREATE TABLE IF NOT EXISTS ");
         request.append(((Table) myClass.getAnnotation(Table.class)).title() + " (");
         Field[] fields = Game.class.getDeclaredFields();
         for(int i = 0; i < fields.length; i++) {
