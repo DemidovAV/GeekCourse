8b3b7a8a65899b6e6cd5b7ae9decefeb5464242c
DemidovAV <demidovavspb@gmail.com>
2023-03-30 17:16:06 +0300

    git commit

---
+++ lesson15/src/main/java/com/example/demospring/controllers/ProductsController.java
@@ -0,0 +1,88 @@
+package com.example.demospring.controllers;
+
+
+import com.example.demospring.entities.Product;
+import com.example.demospring.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/products")
+public class ProductsController {
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String showProductList(Model model,
+                                  @RequestParam(value = "filter", required = false) String filter,
+                                  @RequestParam(value = "min", required = false) Integer min,
+                                  @RequestParam(value = "max", required = false) Integer max) {
+        Product product = new Product();
+        model.addAttribute("products", productService.getAllProductsFiltered(filter, min, max));
+        model.addAttribute("product", product);
+        model.addAttribute("filter", filter);
+        model.addAttribute("filter", min);
+        model.addAttribute("filter", max);
+        return "products";
+    }
+    @PostMapping
+    public String showProductListReset(Model model) {
+        Product product = new Product();
+        String filter = null;
+        Integer min = null;
+        Integer max = null;
+        model.addAttribute("products", productService.getAllProductsFiltered(filter, min, max));
+        model.addAttribute("product", product);
+        model.addAttribute("filter", filter);
+        model.addAttribute("filter", min);
+        model.addAttribute("filter", max);
+        return "products";
+    }
+    @GetMapping("/edit/{id}")
+    public String showEditProduct(Model model, @PathVariable(value="id") Long id){
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "products-edit";
+    }
+
+    @PostMapping("/edit/confirm")
+    public String editConfirm(@ModelAttribute(value="product") Product product) {
+        productService.saveEditedProduct(product);
+        return "redirect:/products";
+    }
+
+
+    @GetMapping("/add")
+    public String addProduct(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "products-add";
+    }
+
+    @PostMapping("/add/confirm")
+    public String addConfirm(@ModelAttribute(value="product") Product product) {
+        productService.saveProduct(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/show_product/{id}")
+    public String showOneProduct(Model model, @PathVariable(value="id") Long id) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "product_page";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(value="id") Long id) {
+        productService.deleteProductById(id);
+        return "redirect:/products";
+    }
+
+
+}
---
+++ lesson15/src/main/java/com/example/demospring/services/ProductService.java
@@ -0,0 +1,59 @@
+package com.example.demospring.services;
+
+import com.example.demospring.entities.Product;
+import com.example.demospring.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProductsFiltered(String filter, Integer min, Integer max) {
+        List<Product> preFilteredList = productRepository.finAll();
+        List<Product> afterFilteredList;
+        if (filter != null) {
+            afterFilteredList = preFilteredList.stream().filter(p-> p.getTitle().toLowerCase().contains(filter.toLowerCase())).collect(Collectors.toList());
+        } else {
+            afterFilteredList = preFilteredList;
+        }
+        if (min != null && max != null) {
+            return afterFilteredList.stream().filter(product -> product.getPrice() >= min && product.getPrice() <= max).collect(Collectors.toList());
+        } else if (min == null && max != null) {
+            return afterFilteredList.stream().filter(product -> product.getPrice() <= max).collect(Collectors.toList());
+        } else if (min != null && max == null) {
+            return afterFilteredList.stream().filter(product -> product.getPrice() >= min).collect(Collectors.toList());
+        } else {
+            return afterFilteredList;
+        }
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product getProductByTitle(String title) {
+        return productRepository.findByTitle(title);
+    }
+
+    public void saveProduct(Product product) {
+        productRepository.saveProduct(product);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteProductById(id);
+    }
+
+    public void saveEditedProduct(Product product) {
+        productRepository.saveEditedProduct(product);
+    }
+
+}
